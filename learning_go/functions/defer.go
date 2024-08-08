5c9b1dceb7905696bcceb899910f3079372c396a
kimathikim <briankimathi94@gmail.com>
2024-08-09 01:59:01 +0300

    stack to string

--- learning_go/functions/defer.go
+++ learning_go/functions/defer.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func loopA() {
 	// Defer statements are executed in LIFO
@@ -35,7 +38,48 @@ func twoAscNum(s int, d int) (j int, k int) {
 	}
 }
 
+// stack function
+// implement pop, push.
+// It should be LIFOs
+func push(s int, stack *[5]int) {
+	for key, value := range stack {
+		if value == 0 {
+			stack[key] = s
+			return
+		}
+	}
+}
+
+func pop(stack *[5]int) {
+	if stack[0] == 0 {
+		fmt.Println("Stack Empty")
+	}
+	for i := len(stack) - 1; i >= 0; i-- {
+		if stack[i] != 0 {
+			stack[i] = 0
+			break
+		}
+	}
+	fmt.Println(*stack)
+}
+
+// finction that converts an array to a string
+func arrString(arr [5]int) string {
+	var str string
+	for key, val := range arr {
+		str += "[" + strconv.Itoa(key) + "] : " + strconv.Itoa(val) + "\n"
+	}
+	return str
+}
+
 func main() {
+	var stack [5]int
+	fmt.Println(stack)
+	(push(3, &stack))
+	fmt.Println(stack)
+
+	fmt.Println(arrString(stack))
+	print("\n")
 	fmt.Println(floatAv([]float64{1.3, 40.23}))
 	fmt.Println(twoAscNum(3, 1))
 	loopA()
